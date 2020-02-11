bc915a9baa3004a3df4f41a65da31ee1548201aa
lucyhe <lucyhe@DSSNITSESYCX7H2.ads.northwestern.edu>
2020-02-11 14:23:26 -0600

    .

--- Base64Encoding/src/northwestern_ses_base64EncodingDecoding/NWSESBase64.java
+++ Base64Encoding/src/northwestern_ses_base64EncodingDecoding/NWSESBase64.java
@@ -30,11 +30,7 @@ public static void main(String[] args) throws IOException {
 <<<<<<< HEAD
     File in_f = new File("C:/Temp/MainRequirements_Orig3.txt");
     PrintStream out_f = new PrintStream(new FileOutputStream("MainRequirements_Orig3_new"));
-=======
-    File in_f = new File("C:/Temp/MainRequirements_Orig5.txt");
-    PrintStream out_f = new PrintStream(new FileOutputStream("MainRequirements_Orig_new"));
->>>>>>> branch 'develop_Lucy' of https://github.com/ses-lucyhe/TGS-Forms.git
-    
+
     //File in_f = new File("C:/Temp/AAR_LongDescr.txt");
     //PrintStream out_f = new PrintStream(new FileOutputStream("C:/Temp/AAR_LongDescr_new.txt"));
     
